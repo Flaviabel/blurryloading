7be433b1e33cab6a00aee0ea0a66127369cb0bf0
Flaviabel <flaviamanfredi521@gmail.com>
2022-11-24 12:55:28 +0100

    add counter opacity

--- main.js
+++ main.js
@@ -2,21 +2,18 @@ loading = document.querySelector('.loading-text');
 bg = document.querySelector('.bg');
 contador = 0;
 intervalo = setInterval(porcentaje, 20);
-// desvanecer= setInterval(desvaneciendo,30);
 
 function porcentaje() {
     contador++;
     if (contador > 99) {
         clearInterval(intervalo);
     }
+    if (contador>60) {
+        loading.style.opacity= scale(contador,0,100,1,0,5)
+    }
     loading.innerText = `${contador}%`;
     bg.style.opacity = scale(contador,0,100,0,1)
 }
-
-// function desvaneciendo() {
-//     if(contador>99){
-//     }
-// }
 
 function scale (number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
